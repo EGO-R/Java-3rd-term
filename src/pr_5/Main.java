f4e4ae856894129ff12de2672bfcc6aec22e9f98
EGO-R <egor_trusov00@mail.ru>
2023-09-25 21:11:37 +0300

    pr_5_1.2

--- src/pr_5/Main.java
+++ src/pr_5/Main.java
@@ -7,16 +7,18 @@ import java.awt.event.ActionListener;
 public class Main {
     public static int MilanScore = 0;
     public static int MadridScore = 0;
-    public static JLabel score = new JLabel("Result: 0 X 0");
-    public static JLabel scorer = new JLabel("Last Scorer: N/A");
-    public static JLabel winner = new JLabel("Winner: DRAW");
+    public static JLabel score = new JLabel("Result: 0 X 0", JLabel.CENTER);
+    public static JLabel scorer = new JLabel("Last Scorer: N/A", JLabel.LEFT);
+    public static JLabel winner = new JLabel("Winner: DRAW", JLabel.RIGHT);
     public static void main(String[] args) {
         JFrame frame = new JFrame("Football");
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300,300);
         JButton button1 = new JButton("AC Milan");
+        button1.setBounds(5, 5, 100, 30);
         JButton button2 = new JButton("“Real Madrid");
+        button2.setBounds(5, 5, 100, 30);
 
 
 
@@ -24,16 +26,32 @@ public class Main {
             @Override
             public void actionPerformed(ActionEvent e) {
                 MilanScore++;
+                score_update();
+                scorer_update("AC Milan");
+                winner_update();
+            }
+        });
 
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MadridScore++;
+                score_update();
+                scorer_update("Real Madrid");
+                winner_update();
             }
         });
 
         JPanel panel = new JPanel();
-        panel.setBackground(Color.BLUE);
-        panel.setPreferredSize(new Dimension(200, 300));
+        panel.setBackground(Color.LIGHT_GRAY);
+        panel.setPreferredSize(new Dimension(500, 300));
+
 
         panel.add(button1);
         panel.add(button2);
+        panel.add(score);
+        panel.add(scorer);
+        panel.add(winner);
         frame.getContentPane().add(panel);
         frame.pack();
         frame.setVisible(true);
@@ -41,5 +59,21 @@ public class Main {
 
     public static void score_update() {
         score.setText("Result: " + MilanScore + " X " + MadridScore);
+        score.revalidate();
+    }
+
+    public static void scorer_update(String s) {
+        scorer.setText("Last Scorer: " + s);
+        score.revalidate();
+    }
+
+    public static void winner_update() {
+        if (MilanScore > MadridScore)
+            winner.setText("Winner: AC Milan");
+        else if (MilanScore < MadridScore)
+            winner.setText("Winner: Real Madrid");
+        else
+            winner.setText("Winner: DRAW");
+        score.revalidate();
     }
 }
