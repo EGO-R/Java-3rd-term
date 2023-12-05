6d1856ff8d54aaddf0ab030492f24e7314e83d27
EGO-R <egor_trusov00@mail.ru>
2023-12-05 23:23:11 +0300

    pr_6_1

---
+++ src/pr_6/T1/Movable.java
@@ -0,0 +1,8 @@
+package pr_6.T1;
+
+public interface Movable {
+    public  void moveUp();
+    public  void moveDown();
+    public  void moveLeft();
+    public  void moveRight();
+}
---
+++ src/pr_6/T1/MovablePoint.java
@@ -0,0 +1,37 @@
+package pr_6.T1;
+
+import pr_6.T1.Movable;
+
+public class MovablePoint implements Movable {
+    protected int x;
+    protected int y;
+    protected int xSpeed;
+    protected int ySpeed;
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "x=" + x +
+                ", y=" + y +
+                ", xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                '}';
+    }
+    public void moveUp(){
+        y += ySpeed;
+    }
+    public void moveDown(){
+        y -= ySpeed;
+    }
+    public void moveRight(){
+        x += xSpeed;
+    }
+    public void moveLeft(){
+        x -= xSpeed;
+    }
+}
